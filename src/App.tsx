2c05cb0b6663c842c6172bcda7fe06e478bee491
ValkoBob <valko_bob@ukr.net>
2020-08-12 12:38:13 +0300

    Remake last problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,68 @@
 import React, { useRef, useState } from 'react';
 import './App.scss';
 
-function App() {
+const App: React.FC = () => {
   const [dragOver, setDragOver] = useState(false);
-  const [fileUpload, setFile] = useState<any | null>(null);
 
-  const fileRef = useRef<any | null>(null);
-
-  const handleDragEnter = (e: React.DragEvent<HTMLInputElement>) => {
+  const fileRef = useRef<HTMLInputElement | null>(null);
+  const [fileUpload, setFile] = useState<typeof fileRef.current | null | File | undefined>(null);
+  const handleDragEnter = (e: React.DragEvent<HTMLInputElement>): void => {
     e.preventDefault();
   };
 
-  const handleDragOver = (e: React.DragEvent<HTMLInputElement>) => {
+  const handleDragOver = (e: React.DragEvent<HTMLInputElement>): void => {
     e.preventDefault();
     if (!dragOver) {
       setDragOver(true);
     }
   };
 
-  const handleDragLeave = (e: React.DragEvent<HTMLInputElement>) => {
+  const handleDragLeave = (e: React.DragEvent<HTMLInputElement>): void => {
     e.preventDefault();
     setDragOver(false);
   };
 
-  const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
+  const handleDrop = (e: React.DragEvent<HTMLInputElement>): void => {
     e.preventDefault();
     const file = e.dataTransfer.files[0];
     setFile(file);
   };
 
-  const handleAddFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleAddFile = (e: React.ChangeEvent<HTMLInputElement>): void => {
     e.preventDefault();
-    const file = fileRef.current.files[0];
-    setFile(file);
+    if (fileRef.current !== null) {
+      const file = fileRef.current.files![0];
+      setFile(file);
+    }
   };
 
-  const handleCancelUpload = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleCancelUpload = (e: React.MouseEvent<HTMLButtonElement>): void => {
     e.preventDefault();
     setFile(null);
   };
 
-  const uploadText = fileUpload
-    ? <div className="form-files">
-            <h4>{
-                fileUpload.name
-            }</h4>
-            <button
-                onClick={handleCancelUpload}
-            >
-                Cancel
-            </button>
-            <input ref={fileRef}
-                   id="uploadNewFile"
-                   type="file"
-                   onChange={handleAddFile}/>
-            <label htmlFor="uploadNewFile">Upload</label>
-        </div>
-    : <>
-            <h2>Drop files here</h2>
-            <span>Or</span>
-            <form action="">
-                <input type="file"
-                       ref={fileRef}
-                       id="upload"
-                       name="upload"
-                       onChange={handleAddFile}
-                />
-                <label htmlFor="upload">Select Files</label>
-            </form>
-        </>;
-
   return (
-        <div className="form"
-             onDrop={handleDrop}
-             onDragEnter={handleDragEnter}
-             onDragOver={handleDragOver}
-             onDragLeave={handleDragLeave}>
-            {uploadText}
+    <div className="form"
+         onDrop={handleDrop}
+         onDragEnter={handleDragEnter}
+         onDragOver={handleDragOver}
+         onDragLeave={handleDragLeave}>
+      {fileUpload ? <div className="form-files">
+          <h4>{fileUpload.name}</h4>
+          <button onClick={handleCancelUpload}>Cancel</button>
+          <input ref={fileRef} id="uploadNewFile" type="file" onChange={handleAddFile}/>
+          <label htmlFor="uploadNewFile">Upload</label>
         </div>
+        : <>
+          <h2>Drop files here</h2>
+          <span>Or</span>
+          <form>
+            <input type="file" ref={fileRef} id="upload" name="upload" onChange={handleAddFile}/>
+            <label htmlFor="upload">Select Files</label>
+          </form>
+        </>}
+    </div>
   );
-}
+};
 
 export default App;
